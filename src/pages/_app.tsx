63867366da47290a08349526c38db172b5aaabb0
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-11 22:57:27 +0900

    chore: setup eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
-import React from "react"
-import * as MU from '@material-ui/core'
+import React from 'react'
 import { AppProps } from 'next/app'
 
 import Header from '../components/organisms/Header'
 
 const App:React.FC<AppProps> = ({Component, pageProps})  => (
   <>
-  <Header />
-  <Component {...pageProps} />
-</>
-  )
+    <Header />
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
