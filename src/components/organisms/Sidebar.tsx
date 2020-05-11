c08bddfbab237441170b27936c7f2b4e0515d85e
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-12 00:43:48 +0900

    feat: add sidebar

---
+++ src/components/organisms/Sidebar.tsx
@@ -0,0 +1,25 @@
+import React from'react'
+import * as MU from '@material-ui/core'
+import {connect} from 'react-redux'
+
+import {sidebarSlice} from '../../store/sidebar'
+
+type Props = {
+  open:boolean,
+  toggle: () => void
+}
+
+const Sidebar : React.FC<Props> = props => (
+  <MU.SwipeableDrawer open={props.open} onClose={props.toggle} onOpen={() => {}}>
+    <div style={{width: 250}}>
+      <MU.List>
+        <MU.ListItemText primary="hoge" />
+      </MU.List>
+    </div>
+  </MU.SwipeableDrawer>
+)
+
+export default connect(
+  (state) => ({ open: state.sidebar.open }),
+  (dispatch) => ({ toggle: () => dispatch(sidebarSlice.actions.toggle()) })
+)(Sidebar)
